19bcc94b4650a07d26a758e0bc7da5a13c07e93a
Mounica Chikkam <mounicachikkam@mounicas-mbp.lan>
2018-07-12 09:18:54 -0700

    add compose tweet|refactored code

---
+++ app/src/main/java/com/mounica/mocktweets/utils/Constants.java
@@ -0,0 +1,15 @@
+package com.mounica.mocktweets;
+
+/**
+ * Global Constants
+ */
+
+public class Constants {
+
+  public static final String CONSUMER_KEY = "HTLXAzAd8JkGn0Km3YvRxBzeS";
+  public static final String CONSUMER_SECRET = "xo1lbsXINsaZlB25AD18mCf7KX5kXGWZpduOAbEqdL9X62cA8A";
+  public static final String ACCESS_TOKEN = "1012031282098757632-36DfTuiWsjG0o8PdmmjkY5ryH7f0SC";
+  public static final String ACCESS_TOKEN_SECRET = "UDajSZcn4C4MhnN1xXObb3m6MrIwsH1Sn6WnhaE8U399j";
+  public static final String BASE_URL_OAUTH_AUTHENTICATE = "https://api.twitter.com/oauth/authenticate/";
+
+}
